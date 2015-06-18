72bfcc1a3a8dd515214343dc5a13fd4df03a75d0
KensoDev <avi@kensodev.com>
2015-06-18 12:35:04 -0700

    Adding Round Robin load balancing

--- proxy.go
+++ proxy.go
@@ -1,11 +1,10 @@
-package main
+package proxy
 
 import (
 	"bytes"
-	"fmt"
-	"log"
+	"github.com/mailgun/oxy/forward"
+	"github.com/mailgun/oxy/roundrobin"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 )
 
@@ -18,27 +17,35 @@ func main() {
 }
 
 type Proxy struct {
-	proxy http.Handler
-	err   error
+	proxy  http.Handler
+	lb     *roundrobin.RoundRobin
+	master string
+	err    error
 }
 
 type RequestReader struct {
 	*bytes.Buffer
 }
 
+func NewProxy(master string, slaves ...string) *Proxy {
+	// oxy lb from slaves
+	fwd, err := forward.New()
+	lb, err := roundrobin.New(fwd)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, slave := range slaves {
+		slaveUrl, err := url.Parse(slave)
+		if err != nil {
+			panic(err)
+		}
+		lb.UpsertServer(slaveUrl)
+	}
+
+	return &Proxy{master: master, lb: lb}
+}
+
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	u, err := url.Parse("http://192.168.99.98:8983")
-	if err != nil {
-		p.err = err
-		return
-	}
-
-	contents, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-
-	log.Printf("request: %s", contents)
-	p.proxy = httputil.NewSingleHostReverseProxy(u)
-	p.proxy.ServeHTTP(w, req)
+	p.lb.ServeHTTP(w, req)
 }
