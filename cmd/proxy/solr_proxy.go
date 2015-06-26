e5e8e0e845ca9c30a7d14362b2aab0d18781a8d7
KensoDev <avi@kensodev.com>
2015-06-25 19:42:39 -0700

    Adding args, not working on passing them in yet

--- cmd/proxy/solr_proxy.go
+++ cmd/proxy/solr_proxy.go
@@ -12,6 +12,8 @@ var (
 	listenPort = kingpin.Flag("listen-port", "Which port should the proxy listen on").String()
 	master     = kingpin.Flag("master", "Location to your master server").String()
 	slaves     = kingpin.Flag("slaves", "Comma separated list of servers that act as slaves").String()
+	awsRegion  = kingpin.Flag("aws-region", "Which AWS region should it use for the cache").Default("us-west-2").String()
+	s3EndPoint = kingpin.Flag("aws-endpoint", "AWS Endpoint for s3").Default("https://s3-us-west-2.amazonaws.com").String()
 )
 
 func main() {
